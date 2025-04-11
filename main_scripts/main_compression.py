1e9ae056a530967852456aab55ac3b9a3063cf23
Arianna Stropeni <ari.stropeni01@gmail.com>
2025-04-11 18:42:09 +0200

    Implemented random choice of features

--- main_scripts/main_compression.py
+++ main_scripts/main_compression.py
@@ -4,8 +4,9 @@ import os
 import random
 import io
 import numpy as np
-import argparse
 from PIL import Image
+import argparse
+import sys
 
 from moviad.datasets.mvtec.mvtec_dataset import MVTecDataset
 from moviad.utilities.custom_feature_extractor_trimmed import CustomFeatureExtractor
@@ -14,7 +15,7 @@ from moviad.models.patchcore.product_quantizer import ProductQuantizer
 
 
 def compress_features(dataset_path, categories, device, backbone, layer_idxs, compression_method, 
-                      quality, feature_dtype, pq_method, pq_subspaces, centroids_per_subspace):
+                      quality, feature_dtype, pq_method, pq_subspaces, centroids_per_subspace, random_sampling, sampling_ratio):
     
     def preprocess_image(image):
         transform = transforms.Compose([transforms.Resize((224, 224)),
@@ -33,6 +34,17 @@ def compress_features(dataset_path, categories, device, backbone, layer_idxs, co
                 for channel in fmap:
                     vectors.setdefault(idx, []).append(channel.reshape(-1).numpy())
         return vectors
+
+    def sample_channels(features, ratio = 0.25):
+        reduced_features = []
+        for f in features:
+            B, C, H, W = f.shape
+            k = int(C * ratio) #number of channels to keep
+            sampled_idxs = torch.tensor(random.sample(range(C), k), device = f.device)
+            sampled_f = torch.index_select(f, dim = 1, index = sampled_idxs)
+            reduced_features.append(sampled_f.flatten())
+        return torch.cat(reduced_features)
+
     
     print(f"Compressing images using {compression_method} with quality of {quality}")
     print(f"Casting features to {feature_dtype}")
@@ -65,7 +77,10 @@ def compress_features(dataset_path, categories, device, backbone, layer_idxs, co
 
             #feature extraction
             features = feature_extractor(image_tensor) #list of [1, C, H, W]
-            flat_features = torch.cat([f.flatten() for f in features]).to(dtype=feature_dtype)
+            if random_sampling:
+                flat_features = sample_channels(features, sampling_ratio).to(dtype = feature_dtype)
+            else:
+                flat_features = torch.cat([f.flatten() for f in features]).to(dtype=feature_dtype)
             features_list.append(flat_features.cpu())
             all_features.append((original_image, features, flat_features))
 
@@ -80,7 +95,7 @@ def compress_features(dataset_path, categories, device, backbone, layer_idxs, co
             feature_matrix = torch.stack(features_list)  
             print(f"Training global PQ with {feature_matrix.shape[0]} vectors...")
             pq.fit(feature_matrix)  
-        else:
+        elif pq_method == "layer" or pq_method == "layer-channel":
             trained_quantizers = {}
             for idx, vectors in feature_vectors.items():
                 print(f"Training PQ for layer {idx} with {len(vectors)} vectors...")
@@ -110,7 +125,7 @@ def compress_features(dataset_path, categories, device, backbone, layer_idxs, co
             if pq_method == "global":
                 compressed_feature = pq.encode(flat_features.unsqueeze(0)) 
                 pq_size = compressed_feature.numel() * compressed_feature.element_size()
-            else:
+            elif pq_method == "layer" or pq_method == "layer-channel":
                 for layer_idx, feature_map in zip(layer_idxs, features):
                     feature_map = feature_map.squeeze(0).cpu()
                     if pq_method == "layer":
@@ -137,6 +152,8 @@ def compress_features(dataset_path, categories, device, backbone, layer_idxs, co
         if pq_method:
             print(f"Average PQ feature size: {int(np.mean(sizes["quantized_features"]))} bytes")
             print_comparisons(f"Compression ratio using PQ features", "features", "quantized_features")
+        
+        print(f"Size of the product quantizers: {sys.getsizeof(trained_quantizers)} bytes")
 
     return
 
@@ -157,9 +174,11 @@ def main():
     parser.add_argument("--compression_method", type = str, default = "JPEG", help = "Compression method to use on images, e.g. JPEG, PNG, ...")
     parser.add_argument("--quality", type = int, default = 50, help = "Amount of compression to be applied to the image when compressing with JPEG")
     parser.add_argument("--feature_dtype", type = str, default = "float32", help = "Data type of the features")
-    parser.add_argument("--pq_method", type = str, default = "None", help = "Choose whether to perform product quantization globally or layer-wise. By default it doesn't apply compression")
+    parser.add_argument("--pq_method", type = str, default = None, help = "Choose whether to perform product quantization globally, layer-wise or channel- and layer-wise. By default it doesn't apply compression")
     parser.add_argument("--pq_subspaces", type = int, help = "Number of subspaces to use in product quantization")
     parser.add_argument("--centroids_per_subspace", type = int, default = 256, help = "Number of centroids per subspace to be used in PQ") 
+    parser.add_argument("--random_sampling", action = "store_true", help = "Applies random sampling of the features to reduce the dimensionality")
+    parser.add_argument("--sampling_ratio", type = float, default = 0.25, help = "Percentage of features that have to be kept")
     parser.add_argument("--seed", type = int, default = 1, help = "Execution seed")
 
     args = parser.parse_args()
@@ -174,7 +193,7 @@ def main():
     "bfloat16": torch.bfloat16,
     "float64": torch.float64}
 
-    pq_methods = ["global", "layer", "layer-channel"]
+    pq_methods = [None, "global", "layer", "layer-channel"]
 
     if args.feature_dtype not in dtype_mapping:
         raise ValueError(f"Unsupported feature_dtype: {args.feature_dtype}. Choose from {list(dtype_mapping.keys())}")
@@ -185,7 +204,7 @@ def main():
     args.feature_dtype = dtype_mapping[args.feature_dtype]
 
     compress_features(args.dataset_path, args.categories, device, args.backbone, args.layer_idxs, args.compression_method, 
-                      args.quality, args.feature_dtype, args.pq_method, args.pq_subspaces, args.centroids_per_subspace)
+                      args.quality, args.feature_dtype, args.pq_method, args.pq_subspaces, args.centroids_per_subspace, args.random_sampling, args.sampling_ratio)
 
 
 if __name__ == "__main__":
