2f88affb2b6b3ab684bbaa9da3aa2f53fc4bd0e3
aristops <ari.stropeni01@gmail.com>
2026-02-24 17:02:33 +0100

    Modified Gitignore

--- moviad/trainers/__init__.py
+++ moviad/trainers/__init__.py
@@ -3,5 +3,5 @@ from .trainer_ganomaly import TrainerGanomaly
 from .trainer_padim import TrainerPadim
 from .trainer_patchcore import TrainerPatchCore
 from .trainer_rd4ad import TrainerRD4AD
-from .trainer_stfpm import TrainerSTFPM
+#from .trainer_stfpm import TrainerSTFPM
 from .trainer_supersimplenet import TrainerSuperSimpleNet
